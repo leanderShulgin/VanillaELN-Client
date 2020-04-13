cf45fba75e9afae8c4497aa77bafa141cd9e78db
Lean <leandrohornos@gmail.com>
2020-04-13 15:08:02 -0300

    variables de campos en nuevo proyecto

---
+++ js/proyecto-nuevo.js
@@ -0,0 +1,15 @@
+var state = {};
+// Campos
+var casTarget = document.getElementById("cas-target");
+var descripcion = document.getElementById("descripcion");
+var id = document.getElementById("id-proyecto");
+var nombre = document.getElementById("nombre-proyecto");
+var pmTarget = document.getElementById("pm-target");
+var responsable = document.getElementById("responsable");
+var smilesTarget = document.getElementById("smiles-target");
+var tipo = document.getElementById("tipo-proyecto");
+
+//Botones
+var btnGuardarProyecto = document.getElementById("btn-guardar-proyecto");
+
+
--- js/proyecto.js
+++
@@ -1,6 +0,0 @@
-// Campos
-var id = document.getElementById("id-proyecto");
-var nombre = document.getElementById("nombre-proyecto");
-var responsable = document.getElementById("responsable-proyecto");
-var mol = document.getElementById("molecula-target");
-var tipo = document.getElementById("tipo-proyecto");
