8e83447505fd0c7d40bf4ec9c0fcb9b15910a996
Lean <leandrohornos@gmail.com>
2020-04-13 15:19:40 -0300

    agrego las funciones de proyecto nuevo

--- js/proyecto-nuevo.js
+++ js/proyecto-nuevo.js
@@ -12,4 +12,52 @@ var tipo = document.getElementById("tipo-proyecto");
 //Botones
 var btnGuardarProyecto = document.getElementById("btn-guardar-proyecto");
 
+//Funciones
 
+function leerTodosLosCampos() {
+  return {
+    casTarget: casTarget.value,
+    descripcion: descripcion.value,
+    id: id.value,
+    nombreProyecto: nombre.value,
+    pmTarget: pmTarget.value,
+    responsable: responsable.value,
+    smilesTarget: smilesTarget.value,
+    tipo: tipo.value,
+  };
+}
+
+function guardarEstado(state) {
+  state = leerTodosLosCampos();
+  console.log("se enviará al servidor el siguiente estado: ");
+  console.log(state);
+}
+
+// ESTA ES LA FUNCION QUE HAY QUE USAR CON LA API:
+
+// function guardarEstado(state) {
+//     state = leerTodosLosCampos();
+//     console.log("se enviará al servidor el siguiente estado: ");
+//     console.log(state);
+//     var data = JSON.stringify(state);
+//     let header = new Headers({
+//       "Access-Control-Allow-Origin": "",
+//       "Content-Type": "application/json",
+//     });
+//     var miInit = {
+//       method: "POST",
+//       body: JSON.stringify(data),
+//       headers: header,
+//       mode: "cors",
+//     };
+//     fetch("http:\\localhost:5000/api/reporte", miInit).then((response) => {
+//       console.log(response);
+//     });
+//   }
+
+//Eventos
+
+btnGuardarProyecto.addEventListener("click", function (e) {
+  e.preventDefault();
+  guardarEstado(state);
+});
