fcd854301844188d33c25e28fe6ea6e50c475d17
Lean <leandrohornos@gmail.com>
2020-04-19 17:49:12 -0300

    funciona tabla reportes activos panel central

--- js/panel-central.js
+++ js/panel-central.js
@@ -1,4 +1,5 @@
 var bodyTablaProyectos = document.getElementById("body-tabla-proyectos");
+var bdyRepAc = document.getElementById("body-tabla-rep-ac");
 // Funciones
 
 function cargarListaProyectos(archivoJSON) {
@@ -36,4 +37,47 @@ function generarFilasProyectos(proyectos) {
   return filas;
 }
 
+function cargarListaReportesActivos(archivoJSON) {
+  fetch(archivoJSON)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log("Cargar reportes pegó la siguiente data:");
+      console.log(data);
+      var filas = generarFilasReportesActivos(data);
+      bdyRepAc.innerHTML = filas;
+    });
+}
+
+function generarFilasReportesActivos(reportes) {
+  filas = "";
+  for (var i = 0; i < reportes.length; i++) {
+    //
+    var urlvars =
+      "?pid=" +
+      reportes[i].encabezado.idProyecto +
+      "&repid=" +
+      reportes[i].encabezado.idReporte;
+    filas +=
+      "<tr>" +
+      "<td><a href='reporte-ver.html" +
+      urlvars +
+      "'>" +
+      reportes[i].encabezado.idProyecto +
+      "</a></td>" +
+      "<td>" +
+      reportes[i].encabezado.idReporte +
+      "</td>" +
+      "<td>" +
+      reportes[i].encabezado.etapa +
+      "</td>" +
+      "</tr>";
+  }
+  return filas;
+}
+
+/* MAIN */
+
 cargarListaProyectos("./demos/proyectos-demo.json");
+cargarListaReportesActivos("./demos/reportes-demo.json");
