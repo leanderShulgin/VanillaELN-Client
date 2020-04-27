8d7f065afbb7eb2123fd00638369b1f3ab74c324
Lean <leandrohornos@gmail.com>
2020-04-27 19:13:05 -0300

    reestructuro el codigo en reporte nuevo

--- js/reporte-nuevo.js
+++ js/reporte-nuevo.js
@@ -1,3 +1,7 @@
+/* En el state se va guardando el estado actual de la planilla. 
+Cuando se le da guarda se actualiza el state y se envía a la base de datos
+via la api. */
+
 var state = {
   encabezado: {},
   objetivo: "",
@@ -5,44 +9,94 @@ var state = {
   reactivos: [],
   seguridad: "",
   equipo: "",
-  journal: [],
+  registros: [],
   resultados: {},
   ambiental: {},
   conclusiones: "",
 };
 
-// INFO GENERAL (ENCABEZADO)
+/* Config */
 
-// Variables
+var header = new Headers({
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+});
+
+/* VARIABLES */
+
+var autor = document.getElementById("autor");
 var encabezado = {}; // Aqui almaceno los datos ingresados
-var idProyecto = document.getElementById("id-proyecto");
-var idReporte = document.getElementById("id-reporte");
 var etapa = document.getElementById("etapa");
 var fecha = document.getElementById("fecha");
-var referencias = document.getElementById("referencias");
-var autor = document.getElementById("autor");
-var responsable = document.getElementById("responsable");
+var numProyecto = document.getElementById("num-proyecto");
+var numReporte = document.getElementById("num-reporte");
 var pmProducto = document.getElementById("pm-producto");
+var referencias = document.getElementById("referencias");
+var responsable = document.getElementById("responsable");
 var via = document.getElementById("via");
+// Campos generales
+var objetivo = document.getElementById("objetivo");
+var reaccion = document.getElementById("reaccion");
+var seguridad = document.getElementById("seguridad");
+// Tabla de reactivos
+var bodyTablaReactivos = document.getElementById("body-tabla-reactivos");
+var equipo = document.getElementById("equipo");
+var limitante = document.getElementById("limitante");
+var masaReactivo = document.getElementById("masa-reactivo");
+var molesReactivo = document.getElementById("moles-reactivo");
+var nombreReactivo = document.getElementById("nombre-reactivo");
+var origenReactivo = document.getElementById("origen-reactivo");
+var pmReactivo = document.getElementById("pm-reactivo");
+var purezaReactivo = document.getElementById("pureza-reactivo");
+var reactivos = []; // Aqui almaceno la tabla de reactivos
+var rmReactivo = document.getElementById("rm-reactivo");
+var tablaReactivos = document.getElementById("tabla-reactivos");
+// Journal
+var registro = document.getElementById("registro");
+var registros = []; // Aqui se almacenan las entradas!
+var visor = document.getElementById("visor-de-registros");
+// Resultados
+var masaTeo = document.getElementById("masa-teorica");
+var mProd = document.getElementById("masa-producto");
+var pProd = document.getElementById("pureza-producto");
+var rend = document.getElementById("rendimiento");
+// Ambiental
+var efAc = document.getElementById("efluentes-acuosos");
+var efOrg = document.getElementById("efluentes-organicos");
+var factorE = document.getElementById("factorE");
+// Conclusiones
+var conclusiones = document.getElementById("conclusiones");
 
+/* BOTONES */
+
+var btnAgregarReactivo = document.getElementById("btn-agregar-reactivo");
+var btnAgregarRegistro = document.getElementById("btn-agregar-entrada");
 var btnEncabezado = document.getElementById("btn-encabezado");
+var btnEquipo = document.getElementById("btn-equipo");
+var btnGuardarEstado = document.getElementById("btn-guardar-estado");
+var btnObjetivo = document.getElementById("btn-objetivo");
+var btnReaccion = document.getElementById("btn-reaccion");
+var btnSeguridad = document.getElementById("btn-seguridad");
 
-btnEncabezado.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (idProyecto.disabled == false) {
-    bloquearEncabezado(true);
-    state.encabezado = leerEncabezado();
-    toggleBtn(btnEncabezado, "off");
+/* FUNCIONES */
+
+function toggleBtn(btn, value) {
+  // Cambia el aspecto de un botón
+  if (value == "on") {
+    btn.innerHTML = "Aceptar";
+    btn.style.backgroundColor = "rgb(49,55,71)";
+    btn.style.color = "rgb(238,236,236)";
   } else {
-    bloquearEncabezado(false);
-    toggleBtn(btnEncabezado, "on");
+    btn.innerHTML = "Editar";
+    btn.style.backgroundColor = "rgb(238,236,236)";
+    btn.style.color = "rgb(49,55,71)";
   }
-});
+}
 
 function leerEncabezado() {
   return {
-    idProyecto: idProyecto.value,
-    idReporte: idReporte.value,
+    numProyecto: numProyecto.value,
+    numReporte: numReporte.value,
     etapa: etapa.value,
     via: via.value,
     pmProducto: pmProducto.value,
@@ -62,11 +116,6 @@ function bloquearEncabezado(trueOrfalse) {
   }
 }
 
-// OBJETIVO-----------------------------
-// variables
-var objetivo = document.getElementById("objetivo");
-var btnObjetivo = document.getElementById("btn-objetivo");
-
 function toggleObjetivo() {
   // Habilita/deshabilita el textarea y cambia e boton
   if (objetivo.disabled === true) {
@@ -77,20 +126,6 @@ function toggleObjetivo() {
     toggleBtn(btnObjetivo, "off");
   }
 }
-
-btnObjetivo.addEventListener("click", function (e) {
-  e.preventDefault();
-  // Salvar estado:
-  state.objetivo = objetivo.value;
-  // Cambiar boton:
-  toggleObjetivo();
-});
-
-// REACCION-----------------------------
-
-// variables
-var reaccion = document.getElementById("reaccion");
-var btnReaccion = document.getElementById("btn-reaccion");
 
 function toggleReaccion() {
   // Habilita/deshabilita el textarea y cambia e boton
@@ -103,37 +138,6 @@ function toggleReaccion() {
   }
 }
 
-btnReaccion.addEventListener("click", function (e) {
-  e.preventDefault();
-  // Salvar estado:
-  state.reaccion = reaccion.value;
-  // Cambiar boton:
-  toggleReaccion();
-});
-
-// REACTIVOS-----------------------------
-
-// variables
-var reactivos = []; // Aqui almaceno la tabla de reactivos
-var tablaReactivos = document.getElementById("tabla-reactivos");
-var bodyTablaReactivos = document.getElementById("body-tabla-reactivos");
-var nombreReactivo = document.getElementById("nombre-reactivo");
-var origenReactivo = document.getElementById("origen-reactivo");
-var masaReactivo = document.getElementById("masa-reactivo");
-var purezaReactivo = document.getElementById("pureza-reactivo");
-var pmReactivo = document.getElementById("pm-reactivo");
-var molesReactivo = document.getElementById("moles-reactivo");
-var rmReactivo = document.getElementById("rm-reactivo");
-var limitante = document.getElementById("limitante");
-var btnAgregarReactivo = document.getElementById("btn-agregar-reactivo");
-
-// eventos y funciones
-btnAgregarReactivo.addEventListener("click", function (e) {
-  e.preventDefault();
-  reactivos.push(nuevoReactivo());
-  bodyTablaReactivos.innerHTML = generarFilasTabla(reactivos);
-});
-
 function nuevoReactivo() {
   return {
     nombre: nombreReactivo.value,
@@ -143,7 +147,7 @@ function nuevoReactivo() {
     pm: pmReactivo.value,
     moles: molesReactivo.value,
     rm: rmReactivo.value,
-    limitante: limitante.value,
+    limitante: false,
   };
 }
 
@@ -179,11 +183,6 @@ function generarFilasTabla(reactivos) {
   return filas;
 }
 
-// SEGURIDAD---------------------------------------
-// variables
-var seguridad = document.getElementById("seguridad");
-var btnSeguridad = document.getElementById("btn-seguridad");
-
 function toggleSeguridad() {
   // Habilita/deshabilita el textarea y cambia e boton
   if (seguridad.disabled === true) {
@@ -194,19 +193,6 @@ function toggleSeguridad() {
     toggleBtn(btnSeguridad, "off");
   }
 }
-
-btnSeguridad.addEventListener("click", function (e) {
-  e.preventDefault();
-  // Salvar estado:
-  state.seguridad = reaccion.value;
-  // Cambiar boton:
-  toggleSeguridad();
-});
-
-// EQUIPO------------------------------------------
-// variables
-var equipo = document.getElementById("equipo");
-var btnEquipo = document.getElementById("btn-equipo");
 
 function toggleEquipo() {
   // Habilita/deshabilita el textarea y cambia e boton
@@ -219,82 +205,42 @@ function toggleEquipo() {
   }
 }
 
-btnEquipo.addEventListener("click", function (e) {
-  e.preventDefault();
-  // Salvar estado:
-  state.equipo = reaccion.value;
-  // Cambiar boton:
-  toggleEquipo();
-});
-
-// JOURNAL (Registro del experimento)--------------
-
-// variables
-var registros = []; // Aqui se almacenan las entradas!
-var visor = document.getElementById("visor-de-registros");
-var registro = document.getElementById("registro");
-var btnAgregarRegistro = document.getElementById("btn-agregar-entrada");
-
-// PrevenDefault para el botón de submit del form:
-btnAgregarRegistro.addEventListener("click", function (e) {
-  e.preventDefault();
-  handleNuevoRegistro();
-});
-
-function handleNuevoRegistro() {
+function nuevoRegistro() {
   /* Maneja el agregado de una nueva entrada al registro de experimentos
       y agrega una etiqueta de fecha y hora al registro.
   */
-  var ahora = fechaHora();
-  registros.push({ hora: ahora, texto: registro.value });
-  visor.innerHTML = "";
-  registro.value = "";
-  for (var i = 0; i < registros.length; i++) {
+  var ahora = new Date();
+  state.registros.push({ hora: ahora, texto: registro.value });
+  visor.innerHTML = ""; //limpio el visor
+  registro.value = ""; //limpio el campo
+  for (var i = 0; i < state.registros.length; i++) {
+    // regenero los posteos desde registros:
     visor.innerHTML +=
       "<p>" +
       '<span class="timeStamp">' +
-      registros[i].hora +
+      state.registros[i].hora +
       " | </span>" +
-      registros[i].texto +
+      state.registros[i].texto +
       "</p>";
   }
 }
-
-// RESULTADOS -----------------------------------
-
-// variables
-
-var mProd = document.getElementById("masa-producto");
-var pProd = document.getElementById("pureza-producto");
-var masaTeo = document.getElementById("masa-teorica");
-var rend = document.getElementById("rendimiento");
 
 function leerResultados() {
   return {
     masaProducto: mProd.value,
     purezaProducto: pProd.value,
+    masaTeorica: masaTeo.value,
     rendimiento: rend.value,
   };
 }
 
-// AMBIENTAL ------------------------------------
-
-// variables
-
-var efOrg = document.getElementById("efluentes-organicos");
-var efAc = document.getElementById("efluentes-acuosos");
-var factorE = document.getElementById("factorE");
-
 function leerAmbiental() {
   return {
-    efluentesOrganicos: efOrg.value,
-    efluentesAcuosos: efAc.value,
+    efsOrganicos: efOrg.value,
+    efsAcuosos: efAc.value,
+    factorE: factorE.value,
   };
 }
-
-// CONCLUSIONES
-var conclusiones = document.getElementById("conclusiones");
-
 
 function leerTodosLosCampos() {
   return {
@@ -304,31 +250,24 @@ function leerTodosLosCampos() {
     reactivos: reactivos,
     seguridad: seguridad.value,
     equipo: equipo.value,
-    journal: registros,
+    registros: registros,
     resultados: leerResultados(),
     ambiental: leerAmbiental(),
     conclusiones: conclusiones.value,
   };
 }
 
-var btnGuardarEstado = document.getElementById("btn-guardar-estado");
-btnGuardarEstado.addEventListener("click", function (e) {
-  e.preventDefault();
-  guardarEstado(state);
-});
-
-function guardarEstado(state) {
+function actualizarState() {
   state = leerTodosLosCampos();
-  console.log("se enviará al servidor el siguiente estado: ");
-  console.log(state);
-  var data = JSON.stringify(state);
-  let header = new Headers({
-    "Access-Control-Allow-Origin": "",
-    "Content-Type": "application/json",
-  });
+  console.log("Nuevo State: ", state);
+}
+
+function guardar() {
+  actualizarState();
+  console.log("se va a guardar lo siguiente: ", state);
   var miInit = {
     method: "POST",
-    body: JSON.stringify(data),
+    body: JSON.stringify(state),
     headers: header,
     mode: "cors",
   };
@@ -337,19 +276,82 @@ function guardarEstado(state) {
   });
 }
 
-function cargarArchivoJSON(archivoJSON) {
-  //lee un archivo json y devuelve un objeto js
-  fetch(archivoJSON).then((data) => {
-    console.log("data cargada del json");
-    return data;
+function createProyecto(state) {
+  // Levanta la info de los campos y crea un nuevo doc en la base de datos
+  state = leerTodosLosCampos();
+  console.log("se enviará al servidor el siguiente estado: ");
+  console.log(state);
+  var miInit = {
+    method: "POST",
+    body: JSON.stringify(state),
+    headers: header,
+    mode: "cors",
+  };
+  fetch("http:\\localhost:5000/api/proyecto", miInit).then((response) => {
+    console.log("respuesta del servidor: ", response);
   });
 }
 
-function cargarData(archivoJSON) {
-  //Toma un objeto js con el contenido de un reporte
-  //y los carga en la interfaz de lectura:
-  console.log("se va a cargar el archivo json");
-  var data = cargarArchivoJSON(archivoJSON);
-  console.log("Se ha cargado la data");
-  console.log(data);
-}
+/* EVENTOS */
+
+btnReaccion.addEventListener("click", function (e) {
+  e.preventDefault();
+  // Salvar estado:
+  state.reaccion = reaccion.value;
+  // Cambiar boton:
+  toggleReaccion();
+});
+
+btnObjetivo.addEventListener("click", function (e) {
+  e.preventDefault();
+  // Salvar estado:
+  state.objetivo = objetivo.value;
+  // Cambiar boton:
+  toggleObjetivo();
+});
+
+btnAgregarReactivo.addEventListener("click", function (e) {
+  e.preventDefault();
+  reactivos.push(nuevoReactivo());
+  bodyTablaReactivos.innerHTML = generarFilasTabla(reactivos);
+});
+
+btnEncabezado.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (numProyecto.disabled == false) {
+    //nuevo reporte
+    guardar();
+    bloquearEncabezado(true);
+    toggleBtn(btnEncabezado, "off");
+  } else {
+    //Edicion
+    bloquearEncabezado(false);
+    toggleBtn(btnEncabezado, "on");
+  }
+});
+
+btnSeguridad.addEventListener("click", function (e) {
+  e.preventDefault();
+  // Salvar estado:
+  state.seguridad = reaccion.value;
+  // Cambiar boton:
+  toggleSeguridad();
+});
+
+btnEquipo.addEventListener("click", function (e) {
+  e.preventDefault();
+  // Salvar estado:
+  state.equipo = reaccion.value;
+  // Cambiar boton:
+  toggleEquipo();
+});
+
+btnAgregarRegistro.addEventListener("click", function (e) {
+  e.preventDefault();
+  nuevoRegistro();
+});
+
+btnGuardarEstado.addEventListener("click", function (e) {
+  e.preventDefault();
+  guardar(state);
+});
