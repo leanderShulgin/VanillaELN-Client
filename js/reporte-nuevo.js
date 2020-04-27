dcc154ce567c244228b4a1210dc612a20d4931f6
Lean <leandrohornos@gmail.com>
2020-04-27 20:54:51 -0300

    cambios en crear reporte nuevo

--- js/reporte-nuevo.js
+++ js/reporte-nuevo.js
@@ -10,3 +10,48 @@ var responsable = document.getElementById("responsable");
 var via = document.getElementById("via");
 
 var objetivo = document.getElementById("objetivo");
+
+var btnRegistrar = document.getElementById("btn-registrar");
+
+/* Config */
+
+var header = new Headers({
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+});
+
+function leerDatos() {
+  return {
+    encabezado: {
+      numProyecto: numProyecto.value,
+      numReporte: numReporte.value,
+      etapa: etapa.value,
+      via: via.value,
+      pmProducto: pmProducto.value,
+      fecha: fecha.value,
+      referencias: referencias.value,
+      autor: autor.value,
+      responsable: responsable.value,
+    },
+    objetivo: objetivo.value,
+  };
+}
+
+function crearReporte() {
+  datos = leerDatos();
+  console.log("se va a guardar lo siguiente: ", datos);
+  var miInit = {
+    method: "POST",
+    body: JSON.stringify(datos),
+    headers: header,
+    mode: "cors",
+  };
+  fetch("http:\\localhost:5000/api/reporte", miInit).then((response) => {
+    console.log(response);
+  });
+}
+
+btnRegistrar.addEventListener("click", function (e) {
+  e.preventDefault();
+  crearReporte();
+});
