b2f722895d403a16741f215dbe5debf5521e1950
Lean <leandrohornos@gmail.com>
2020-05-18 20:11:07 -0300

    elimino funciones innecesarias

--- js/composer.js
+++ js/composer.js
@@ -44,6 +44,7 @@ composer.setChemToolButtons(appConfig.chemToolButtons);
 /*---Funciones del Editor--------------------------------*/
 
 // Obtener la info en el editor:
+
 function getFullDocument() {
   //devuelve el objeto completo dibujado en el editor
   var chemDoc = composer.getChemObj();
@@ -58,6 +59,7 @@ function getAllMolecules() {
 }
 
 // Renderizado:
+
 function painterMolecule2D(mol) {
   //Dibuja una molecula en 2 dimensiones
   var renderType = Kekule.Render.RendererType.R2D; //R3D  // do 2D or 3D drawing
@@ -78,52 +80,3 @@ function painterMolecule2D(mol) {
   // at last, draw the molecule at the center of context
   painter.draw(context, { x: dim.width / 2, y: dim.height / 2 });
 }
-
-function renderMolecule2D(mol) {
-  var renderType = Kekule.Render.RendererType.R2D; //R3D  // do 2D or 3D drawing
-  var is3D = renderType === Kekule.Render.RendererType.R3D;
-
-  // parent HTML element, we will draw inside it
-  var parentElem = document.getElementById("render");
-  // clear parent elem
-  //Kekule.DomUtils.clearChildContent(parentElem);
-
-  // Get suitable draw bridge for 2D or 3D drawing
-  var drawBridgeManager = is3D
-    ? Kekule.Render.DrawBridge3DMananger
-    : Kekule.Render.DrawBridge2DMananger;
-  var drawBridge = drawBridgeManager.getPreferredBridgeInstance();
-
-  // then create render context by drawBridge first
-  var dim = Kekule.HtmlElementUtils.getElemOffsetDimension(parentElem);
-  console.log(dim); // get width/height of parent element
-  var context = drawBridge.createContext(parentElem, dim.width, dim.height); // create context fulfill parent element
-
-  // then create suitable renderer to render molecule object
-  var rendererClass;
-  if (is3D) rendererClass = Kekule.Render.get3DRendererClass(mol);
-  else rendererClass = Kekule.Render.get2DRendererClass(mol);
-  var renderer = new rendererClass(mol, drawBridge); // create concrete renderer object and bind it with mol and draw bridge
-
-  /*
-    prepare render options, options is a something like
-
-    var options = {
-    atomColor: '#000000',
-    bondColor: '#000000',
-    defBondLength: 30
-    ...
-    };
-    it is quite complex, so we use some magic here, generate the option object by default configuration of Kekule.js.
-    */
-  var configObj = is3D
-    ? Kekule.Render.Render3DConfigs.getInstance()
-    : Kekule.Render.Render2DConfigs.getInstance();
-  var options = Kekule.Render.RenderOptionUtils.convertConfigsToPlainHash(
-    configObj
-  );
-  console.log(options);
-  // at last, draw the molecule to the center of context
-  renderer.draw(context, { x: dim.width / 2, y: dim.height / 2 }, options);
-}
-
